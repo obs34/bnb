72b3177e792940ce529e5600cbfdb4bf99cf51a1
Jaguin Enzo <ejaguin@herault.fr>
2025-04-25 17:36:50 +0200

    add data notebook

--- data_gen.py
+++ data_gen.py
@@ -1,14 +1,23 @@
+import os
 import pandas as pd
 
-PATH_AIRDNA = 'data/airdna/'
+from dotenv import load_dotenv
+
+load_dotenv()
+
+PATH_DATA_G = os.getenv('PATH_DATA_G')
+G_AIRDNA = 'airdna'
+G_DECRET = 'decret'
+
 print("Chargement des données Airbnb...")
-bnb2022 = pd.read_excel(PATH_AIRDNA + 'offre_airdna_34_2022.xlsx')
+chemin = os.path.join(PATH_DATA_G, G_AIRDNA, 'offre_airdna_34_2022.xlsx')
+bnb2022 = pd.read_excel(chemin)
 bnb2022 = bnb2022[bnb2022['Liste logement']=='Entire home/apt']
 bnb2022 = bnb2022[['INSEE','Communes','Code EPCI','EPCI','Longitude','Latitude','Revenu annuel €',
                    'Nb jours réservés','Nb total jours de disponibilité']]
 
-
-bnb2023 = pd.read_excel(PATH_AIRDNA + 'offre_airdna_34_2023.xlsx')
+chemin = os.path.join(PATH_DATA_G, G_AIRDNA, 'offre_airdna_34_2023.xlsx')
+bnb2023 = pd.read_excel(chemin)
 bnb2023 = bnb2023[bnb2023['Nature logement']=='Entire home/apt']
 bnb2023 = bnb2023[['INSEE','Communes','Code EPCI','EPCI','Longitude','Latitude','Revenu annuel â‚¬',
                              'Nb jours rÃ©servÃ©s','Nb total jours de disponibilitÃ©']]
@@ -16,7 +25,8 @@ bnb2023.columns = ['INSEE','Communes','Code EPCI','EPCI','Longitude','Latitude',
                              'Nb jours réservés','Nb total jours de disponibilité']
 print("Chargement des données Airbnb terminé.")
 
-décret = pd.read_excel("data/décret/2023_decret.xlsx", sheet_name="3")
+chemin = os.path.join(PATH_DATA_G, G_DECRET, "2023_decret.xlsx")
+decret = pd.read_excel(chemin, sheet_name="3")
 
 def tables():
-    return bnb2022, bnb2023, décret
+    return bnb2022, bnb2023, decret
